86f3d12d9faa46ab44a1320119ea8f3f25588950
Jose Asdrubal Rosas Matus <arosas@enera.mx>
2016-05-25 16:47:54 -0500

    se quitaron comentarios

--- conf/config.py
+++ conf/config.py
@@ -1,28 +1,49 @@
-import os
-
-
 class Config(object):
     DEBUG = False
-    HOST = os.environ.get("HOST")
-    PORT = os.environ.get("PORT")
-    SECRET_KEY = os.environ.get("key")
-    MONGO_HOST = 'localhost'
-    MONGO_PORT = '27017'
-    MONGO_DBNAME = 'quotes'
+    # HOST = os.environ.get("HOST")
+    SECRET_KEY = '^*d^032DZQX48221d3ZA'
+    # HOST = '127.0.0.1'
+    # PORT = 5000
+    # MONGO_HOST = 'localhost'
+    # MONGO_PORT = '27017'
+    # MONGO_DBNAME = 'enera'
+    MONGODB_HOST = '127.0.0.1'
+    MONGODB_PORT = 27017
+    MONGODB_DB = 'enera'
+    # MONGODB_USERNAME = ''
+    # MONGODB_PASSWORD = ''
 
 
 # END CONFIG
 
 class LocalConfig(Config):
     DEBUG = True
-    PORT = os.environ.get('PORT')
+    PORT = 5000
+    MONGODB_DB = 'enera'
+    MONGODB_HOST = '127.0.0.1'
+    MONGODB_PORT = 27017
+    MONGODB_USERNAME = 'enera'
+    MONGODB_PASSWORD = 'enera'
+    # MONGO_HOST = 'ds056998.mongolab.com'
+    # MONGO_PORT = 56998
+    # MONGO_DBNAME = 'enera'
+    # MONGO_USERNAME = 'enera'
+    # MONGO_PASSWORD = 'enera'
 
 
 # END LOCALCONFIG
 
 
 class DevelopConfig(Config):
-    HOST = os.environ.get('HOST')
-    MONGO_HOST = os.environ.get('MONGO_HOST')
-    PORT = os.environ.get('PORT')
+    PORT = 5000
+    MONGO_HOST = 'ds056998.mongolab.com'
+    MONGO_PORT = 56998
+    MONGO_DBNAME = 'enera'
+    MONGO_USERNAME = 'enera'
+    MONGO_PASSWORD = 'enera'
+    # HOST = '0.0.0.0'
+    # MONGO_HOST = 'http://dev-hackergarage.ppnvvkijpm.us-west-1.elasticbeanstalk.com/'
+    # HOST = os.environ.get("HOST")
+    # MONGO_HOST = os.environ.get("MONGO_HOST")
+    # PORT = os.environ.get("PORT")
     # END DEVELOP
--- modules/enera.py
+++ modules/enera.py
@@ -4,10 +4,8 @@ import pytz
 from flask import Blueprint, current_app, request, Response, redirect
 from flask.ext.api import status
 from modules import mongo, Clients, CmxRaw, Branch, CmxUrl
-import pprint
 from bson import ObjectId
 import datetime
-import calendar
 
 prefix_module = 'enera'
 enera = Blueprint(prefix_module, __name__)
@@ -24,42 +22,34 @@ def validate(company):
             print('no encontro nada')
             return 'no valido', status.HTTP_404_NOT_FOUND
         else:
+            # print('no esta vacio')
+            # print('--')
             if request.method == 'GET':
-                CmxUrl(url=request.url, metodo=request.method, data=request.data, json=request.json).save()
+                # CmxUrl(url=request.url, metodo=request.method, data=request.data, json=request.json).save()
                 # print('Hello, is post %s' % request.method)
-                token = cliente.meraki['token']
                 # CmxUrl(url=url, metodo=request.method).save()
                 return Response(token, status.HTTP_200_OK, mimetype='text/plain')
             else:  # return 'Hello, is  %s' % request.method
                 if request.method == 'POST':
                     # print('Hello, is post %s' % request.method)
                     # CmxUrl(url=request.url, metodo=request.method, json=request.json).save()
-                    json = request.json
-                    # Cmx(url=request.url, metodo=request.method, data=request.data).save()
                     try:  # valida que sea un json
                         json_info = request.json
-                        # print(json_info)
-                        # print('----')
-                        # pprint.pprint(json_info)
                     except Exception as e:
-                        # print(request.data)
-                        pprint.pprint('2')
+                        # pprint.pprint('2')
                         return {'error': 'It is not JSON information'}, status.HTTP_400_BAD_REQUEST
-                    pprint.pprint('3')
+                    # pprint.pprint('3')
                     if json_info is None:  # valida que no este vacio
                         return {'error': 'It is not JSON information'}, status.HTTP_400_BAD_REQUEST
                     branch = Branch.objects(aps=json_info['data']['apMac']).first()  # se saca a que branch pertenece
-                    # print(branch['id'])
                     ap = {
                         "mac": json_info['data']['apMac'],
                         "tags": json_info['data']['apTags'],
                         "floors": json_info['data']['apFloors'],
                         "branche_id": branch['id'],
                     }
-                    # print('---')
                     devices = json_info['data']['observations']
-                    # print('total de devices ' + str(len(devices)))
-                    tz = pytz.timezone('America/Mexico_City')  # se define la zona horaria
+                    # tz = pytz.timezone('America/Mexico_City')  # se define la zona horaria
                     for cel in devices:  # Second Example
                         device = {
                             "mac": cel['clientMac'],
@@ -75,9 +65,9 @@ def validate(company):
                             "lat_lng": [cel['location']['lat'], cel['location']['lng']],
                             "unc": cel['location']['unc']
                         }
-                        # print('---')
                         CmxRaw(ap=ap, device=device, location=location).save()
-                    return 'total de device se guardaron, %s' % len(devices)
+                    # return 'total de device se guardaron, %s' % len(devices)
+                    return 'ok', status.HTTP_200_OK
     else:
         print('no funciona')
         return 'no encontrado', status.HTTP_404_NOT_FOUND
