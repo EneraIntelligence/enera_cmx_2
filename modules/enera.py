388f65612509a845294912f84800c95119b8550b
Jose Asdrubal Rosas Matus <arosas@enera.mx>
2016-06-08 10:27:37 -0500

    refactor

--- modules/enera.py
+++ modules/enera.py
@@ -39,7 +39,6 @@ def tr(company):
         print('-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+')
         print(e.args[0])
         print('-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+')
-
         logger.error('Failed in enera.py', exc_info=True)
         issues(e, request.url, {"json": ""})
 
@@ -51,91 +50,90 @@ def validate(company):
     lat = float
     lng = float
     unc = float
-    try:
-        if len(company) == 24:
-            # CmxUrl(url=request.url, metodo=request.method).save()
-            com = str(company)
-            cliente = Clients.objects(id=ObjectId(com)).first()
-            if not cliente:
-                print('no encontro cliente')
-                issues('url no valida no se encontro cliente', request.url, {"json": json, "ap": ap})
+
+    if len(company) == 24:
+        # CmxUrl(url=request.url, metodo=request.method).save()
+        com = str(company)
+        cliente = Clients.objects(id=ObjectId(com)).first()
+        if not cliente:
+            print('no encontro cliente')
+            issues('url no valida no se encontro cliente', request.url, {"json": json, "ap": ap})
+            logger.error('Failed in enera.py', exc_info=True)
+            return 'no valido', status.HTTP_404_NOT_FOUND
+        else:
+            print('--')
+            # CmxUrl(url=request.url, metodo=request.method, json=request.json).save()
+            try:  # valida que sea un json
+                json_info = request.json
+            except Exception as e:
+                # pprint.pprint('2')
+                issues('It is not JSON information', request.url, {"json": json, "ap": ap})
                 logger.error('Failed in enera.py', exc_info=True)
-                return 'no valido', status.HTTP_404_NOT_FOUND
+                return {'error': 'information'}, status.HTTP_400_BAD_REQUEST
+            # print('3')
+            if json_info is None:  # valida que no este vacio
+                issues('It is JSON vacio', request.url, {"json": json, "ap": ap})
+                logger.error('Failed in enera.py', exc_info=True)
+                return {'error': ' information'}, status.HTTP_400_BAD_REQUEST
+            print('se saca el branche')
+            # se saca a que branch pertenece
+            branch = Branch.objects(aps=json_info['data']['apMac']).first()
+            if branch:
+                pprint.pprint(branch['id'])
             else:
-                print('--')
-                # CmxUrl(url=request.url, metodo=request.method, json=request.json).save()
-                try:  # valida que sea un json
-                    json_info = request.json
-                except Exception as e:
-                    # pprint.pprint('2')
-                    issues('It is not JSON information', request.url, {"json": json, "ap": ap})
-                    logger.error('Failed in enera.py', exc_info=True)
-                    return {'error': 'information'}, status.HTTP_400_BAD_REQUEST
-                # print('3')
-                if json_info is None:  # valida que no este vacio
-                    issues('It is JSON vacio', request.url, {"json": json, "ap": ap})
-                    logger.error('Failed in enera.py', exc_info=True)
-                    return {'error': ' information'}, status.HTTP_400_BAD_REQUEST
-                print('se saca el branche')
-                # se saca a que branch pertenece
-                branch = Branch.objects(aps=json_info['data']['apMac']).first()
-                if branch:
-                    pprint.pprint(branch['id'])
-                else:
-                    print('no existe el ap en las branche')
-                    logger.error('Failed in enera.py', exc_info=True)
-                    issues('el ap no esta en una branche', request.url, {"json": json, "ap": ap})
-                    return {'error': ' information'}, status.HTTP_400_BAD_REQUEST
-                # bi = str(branch['id'])
-                ap = {
-                    "mac": json_info['data']['apMac'],
-                    "tags": json_info['data']['apTags'],
-                    "floors": json_info['data']['apFloors'],
-                    "branche_id": branch['id'],
-                }
-                print('datos del ap')
-                devices = json_info['data']['observations']
-                # tz = pytz.timezone('America/Mexico_City')  # se define la zona horaria
-                for cel in devices:  # Second Example
-                    device = {
-                        "mac": cel['clientMac'],
-                        "ipv4": cel['ipv4'],
-                        "ipv6": cel['ipv6'],
-                        "last_seen": datetime.datetime.fromtimestamp(cel['seenEpoch'], pytz.utc),
-                        "ssid": cel['ssid'],
-                        "os": cel['os'],
-                        "manufacturer": cel['manufacturer'],
-                        "rssi": cel['rssi'],
-                    }
-                    if cel['location']['lat'] is not None:
-                        lat = cel['location']['lat']
-                        print(lat)
-                        # lat = 0
-                    if cel['location']['lng'] is not None:
-                        lng = cel['location']['lng']
-                        print(lng)
-                        # lng = 0
-                    if cel['location']['unc'] is not None:
-                        unc = cel['location']['unc']
-                        print(unc)
-                        # unc = 0
+                print('no existe el ap en las branche')
+                logger.error('Failed in enera.py', exc_info=True)
+                issues('el ap no esta en una branche', request.url, {"json": json, "ap": ap})
+                return {'error': ' information'}, status.HTTP_400_BAD_REQUEST
+            # bi = str(branch['id'])
+            ap = {
+                "mac": json_info['data']['apMac'],
+                "tags": json_info['data']['apTags'],
+                "floors": json_info['data']['apFloors'],
+                "branche_id": branch['id'],
+            }
+            print('datos del ap')
+            devices = json_info['data']['observations']
+            # tz = pytz.timezone('America/Mexico_City')  # se define la zona horaria
+            device = {
+                "mac": '',
+                "ipv4": '',
+                "ipv6": '',
+                "last_seen": '',
+                "ssid": '',
+                "os": '',
+                "manufacturer": '',
+                "rssi": ''
+            }
+            for cel in devices:  # Second Example
+
+                device['mac'] = cel['clientMac']
+                device['ipv4'] = cel['ipv4']
+                device['ipv6'] = cel['ipv6']
+                device['last_seen'] = datetime.datetime.fromtimestamp(cel['seenEpoch'], pytz.utc)
+                device['ssid'] = cel['ssid']
+                device['os'] = cel['os']
+                device['manufacturer'] = cel['manufacturer']
+                device['rssi'] = cel['rssi']
+
+                location = ''
+
+                if cel['location'] is not None:
+                    lat = cel['location']['lat']
+                    lng = cel['location']['lng']
+                    unc = cel['location']['unc']
                     location = {
                         "lat_lng": [lat, lng],
                         "unc": unc
                     }
-                    json = cel['location']
-                    print('location')
-                    # pprint.pprint(cel['location'])
-                    print('------------------------')
-                    CmxRaw(ap=ap, device=device, location=location).save()
-                # logger.info('total de dispositivos captados, %s')
-                print('total de dispositivos captados, %s' % len(devices))
-                return 'ok', status.HTTP_200_OK
-        else:
-            return 'not found', status.HTTP_404_NOT_FOUND
-    except Exception as e:
-        pprint.pprint(e.args)
-        logger.error('Failed in enera.py', exc_info=True)
-        issues(e, request.url, {"json": json, "ap": ap})
-    finally:  # print('por default')
-        print('//////////////////////////////')
+
+                #json = cel['location']
+                print('location')
+                # pprint.pprint(cel['location'])
+                print('------------------------')
+                CmxRaw(ap=ap, device=device, location=location).save()
+            # logger.info('total de dispositivos captados, %s')
+            print('total de dispositivos captados, %s' % len(devices))
+            return 'ok', status.HTTP_200_OK
+    else:
+        return 'not found', status.HTTP_404_NOT_FOUND
